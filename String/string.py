dbec49c6230172b83b804519e3a88244d48a879f
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-04-10 05:53:05 +0600

    string file upload

---
+++ String/string.py
@@ -0,0 +1,30 @@
+'''
+Strings
+Strings in python are surrounded by either single quotation marks, or double quotation marks.
+'hello' is the same as "hello".
+You can display a string literal with the print() function:
+'''
+print("Hello")
+print('Hello')
+
+# You can use quotes inside a string, as long as they don't match the quotes surrounding the string:
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+# Assigning a string to a variable is done with the variable name followed by an equal sign and the string:
+a = "Hello"
+print(a)
+
+# You can assign a multiline string to a variable by using three quotes Or three single quotes:
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+x = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(x)
