408139997b977cbd628003d63851829b0d51acd3
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-04-09 12:21:00 +0600

    upload data type check using python

---
+++ dataType.py
@@ -0,0 +1,28 @@
+'''Built-in Data Types
+In programming, data type is an important concept.
+
+Variables can store data of different types, and different types can do different things.
+
+Python has the following data types built-in by default, in these categories:
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType'''
+
+
+x = 40
+print(type(x))
+
+y = "Noman"
+print(type(y))
+
+b = True
+print(type(b))
+
+y = 1.990
+print(type(y))
