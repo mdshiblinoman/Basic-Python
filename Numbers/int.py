b8f31c0150a8185eeedc0562eb60f4fb80e233bb
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-04-09 12:26:26 +0600

    upload numbers

---
+++ Numbers/int.py
@@ -0,0 +1,18 @@
+'''
+Python Numbers
+There are three numeric types in Python:
+    int
+    float
+    complex
+'''
+
+
+# Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.
+a = 1
+b = 35656222554887711
+c = -3255522
+
+print(type(a))
+print(type(b))
+print(type(c))
+
