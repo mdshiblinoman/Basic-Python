08178946b5c49bc0c5e3e757c7e29f030d72d5c3
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-04-11 21:39:35 +0600

    escape way

---
+++ String/escape_character.py
@@ -0,0 +1,12 @@
+'''
+Escape Character
+To insert characters that are illegal in a string, use an escape character.
+An escape character is a backslash \ followed by the character you want to insert.
+An example of an illegal character is a double quote inside a string that is surrounded by double quotes:
+'''
+
+# You will get an error if you use double quotes inside a string that is surrounded by double quotes:
+txt = "We are the so-called "Vikings" from the north."
+
+# The escape character allows you to use double quotes when you normally would not be allowed:
+txt = "We are the so-called \"Vikings\" from the north."
