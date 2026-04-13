b050ab40880a32249a8c1dce6613f323370b1d15
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-04-13 06:24:16 +0600

    upload arithmetic operation in python

---
+++ Operators/arithmetic.py
@@ -0,0 +1,17 @@
+# In the example below, we use the +, -, *, /, %, // operator to add together two values:
+a = 7
+b = 9
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Divition
+print(a % b)  # Modulus
+print(a // b) # Floor Division
+print(a ** b)  # Exponentiation
+
+# Although the + operator is often used to add together two values, like in the example above,
+# it can also be used to add together a variable and a value, or two variables
+sum1 = 100 + 50      # 150 (100 + 50)
+sum2 = sum1 + 250    # 400 (150 + 250)
+sum3 = sum2 + sum2   # 800 (400 + 400)
+print(sum3)
